e7d75b789f562b4cbc3165a22aeaa2456b711e7a
Alan Tam <alan@Alans-MacBook-Pro.local>
2021-12-14 11:06:27 -0800

    adds more views and controller updates

--- routes/destinations.js
+++ routes/destinations.js
@@ -1,3 +1,7 @@
 const express = require('express');
-const router = express.router;
+const router = express.Router();
 const destinationsCtrl = require('../controller/destinations');
+
+router.post('/flights/:id/destinations', destinationsCtrl.create);
+
+module.exports = router;
